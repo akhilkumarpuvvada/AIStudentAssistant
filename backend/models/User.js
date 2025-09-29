d4c8347e6e1abcbb9101fd85b9757e8bbc9c1cc7
akhilpuvvada <pnvakhil13@gmail.com>
2025-09-29 12:22:12 +0200

    Create user model

---
+++ backend/models/User.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+
+const userSchema = new mongoose.Schema({
+    _name: {type: String, required: true},
+    email: {type: String, required: true, unique: true},
+    password: {type: String, required: true},
+    role: { type: String, enum: ["student", "teacher", "admin"]},
+    classIds: [{
+        type: mongoose.Schema.Types.ObjectId, ref: "Class"
+    }]
+},
+{timeStamps: true}
+)
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
