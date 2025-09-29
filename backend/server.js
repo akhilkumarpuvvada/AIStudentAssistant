02b44e44d673137e8833d4f1d5f57d9625dfb0ec
akhilpuvvada <pnvakhil13@gmail.com>
2025-09-29 12:22:00 +0200

    Create connection for mongodb

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,14 @@
 import express from "express";
 import cors from "cors";
 import "dotenv/config";
+import { connectDB } from "./config/db.js";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+connectDB()
 
 app.get("/", (req, res) => res.send("Api is working ") )
 
